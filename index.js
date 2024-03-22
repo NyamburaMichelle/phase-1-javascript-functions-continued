e86c3cf865cdd3d2666c9a9bc9b2834ea7a15eb6
Michelle <nyamburamichelle400@gmail.com>
2024-03-22 12:10:24 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+function saturdayFun(activity= "roller-skate"){
+  return`This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+   return `This Monday, I will ${activity}.`
+}
+
+  /*function wrapAdjective(compliment='*') {
+      const xx="a"
+        return function() {
+    const xy="dedicated"
+        return function() {
+    const yy="programmer"
+        return function() {
+    console.log(`${compliment} ${xx} ${xy} ${yy}`)}
+        }          
+  }
+}
+wrapAdjective("You are")()()()*/
+
+function wrapAdjective(compliment='*') {
+       const xx= function(adjective='a beautiful programmer'){
+          return `You are ${compliment}${adjective}${compliment}!`
+       }
+        return xx
+    }
+    const result= wrapAdjective('||')
+
+
+
+
+
+  
